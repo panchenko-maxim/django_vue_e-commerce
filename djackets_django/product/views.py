04ba35802c0068226edd65773a0b36ad86824839
root221 <panchenko.maxim87@gmail.com>
2025-05-12 16:52:30 +0300

    39:27 create searilizers and models

--- djackets_django/product/views.py
+++ djackets_django/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializers
+
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializers(products, many=True)
+        return Response(serializer.data)
